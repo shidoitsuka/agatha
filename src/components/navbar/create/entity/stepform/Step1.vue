0ffc752b0d7154fb5bd28cbfc58f889acd680ae6
Brian Harianja <hi@shdev.tech>
2021-11-03 01:49:11 +0700

    updated cards height

--- src/components/navbar/create/entity/stepform/Step1.vue
+++ src/components/navbar/create/entity/stepform/Step1.vue
@@ -4,7 +4,7 @@
       <h3 class="text-2xl pt-7 font-semibold softgrey-text">Case Templates</h3>
       <h5 class="softgrey-text">Create a case to collect interesting documents that needs to be monitored.</h5>
       <div class="grid gap-4 mt-5 sm:grid-cols-2 md:grid-cols-4">
-        <div class="grid w-40 gap-4 p-5 text-center rounded-lg cursor-pointer bg-base-300 place-items-center hover:bg-success">
+        <div class="grid w-40 gap-4 p-10 text-center rounded-lg cursor-pointer bg-base-300 place-items-center hover:bg-success">
           <div class="avatar">
             <div class="entity-img rounded-full">
               <img src="/icons/person-alt1.svg">
