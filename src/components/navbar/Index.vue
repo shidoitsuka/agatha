446cff24aee2cc6e8815ce28e3fbabd37f405716
Brian Harianja <hi@shdev.tech>
2021-11-08 22:49:44 +0700

    ham menu

--- src/components/navbar/Index.vue
+++ src/components/navbar/Index.vue
@@ -5,6 +5,11 @@
     </div>
     <div class="flex-1 hidden max-w-lg px-2 mx-2 space-x-4 lg:flex">
       <img src="@/assets/logo.png" alt="agatha">
+      <button class="btn btn-square btn-ghost bg-base-300">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  </button>
       <SearchBar />
     </div>
     <div class="flex-none divide-x-2 divide-base-300">
